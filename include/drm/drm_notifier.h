b5732d15ecc8a0964052c34a1871fa114b2a41bd
Alexander Winkowski <dereference23@outlook.com>
2023-08-17 07:46:47 +0000

    techpack: display: Implement DRM notifier
    Change-Id: Ib6d080ac51ce34c26d2fff029698008e0f4366dc
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- include/drm/drm_notifier.h
+++ include/drm/drm_notifier.h
@@ -1,13 +1,14 @@
 #ifndef _DRM_NOTIFIER_H_
 #define _DRM_NOTIFIER_H_
+
 /*
- *	This include file is intended for touch panel that to receive
- *	blank/unblank event.
- *
+ * This include file is intended for touch panel that to receive
+ * blank/unblank event.
  */
-#define		DRM_EARLY_EVENT_BLANK   0x01
-#define		DRM_EVENT_BLANK         0x02
-#define		DRM_R_EARLY_EVENT_BLANK 0x03
+
+#define DRM_EARLY_EVENT_BLANK	0x01
+#define DRM_EVENT_BLANK		0x02
+
 enum {
 	DRM_BLANK_UNBLANK = 0,
 	DRM_BLANK_LP1,
@@ -16,12 +17,13 @@ enum {
 	DRM_BLANK_SUSPEND,
 	DRM_BLANK_POWERDOWN,
 };
+
 struct drm_notify_data {
-	bool is_primary;
 	void *data;
 };
+
 extern int drm_register_client(struct notifier_block *nb);
 extern int drm_unregister_client(struct notifier_block *nb);
 extern int drm_notifier_call_chain(unsigned long val, void *v);
-#endif /*_DRM_NOTIFIER_H*/
 
+#endif /* _DRM_NOTIFIER_H */
